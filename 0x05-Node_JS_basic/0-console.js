c310eb88b12d3ff3474a7f62dd373844220de8cd
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2024-02-27 08:45:12 +0200

    Fix task 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -3,8 +3,8 @@
  * @param {String} msg Message to display.
  * @author abdelaziz mohamed <https://github.com/Abdel-AzizMohamed>
  */
-const displayMessage = (msg) => {
+function displayMessage(msg) {
   console.log(msg);
-};
+}
 
 module.exports = displayMessage;
