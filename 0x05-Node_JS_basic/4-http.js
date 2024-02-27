9678e837d14d5d794b87877b620d61c976d77482
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2024-02-27 09:01:29 +0200

    Fix tassk

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,17 +1,17 @@
-const http = require("http");
+const http = require('http');
 
 const PORT = 1245;
-const HOST = "localhost";
+const HOST = 'localhost';
 const app = http.createServer();
 
-app.on("request", (_, res) => {
-  const response = "Hello Holberton School!";
-
-  res.setHeader("Content-Type", "text/plain");
+app.on('request', (_, res) => {
   res.statusCode = 200;
-  res.end(response);
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(PORT, HOST, () => {
   console.log(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
+
+module.exports = app;
